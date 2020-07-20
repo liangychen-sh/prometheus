5717d791b331741e3f76daa95acbabd767a4efd4
liangychen <liangychen@stubhub.com>
2020-07-20 08:45:27 +0800

    test

--- src/main/java/com/stubhub/promethues/PrometheusApplication.java
+++ src/main/java/com/stubhub/promethues/PrometheusApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 import org.springframework.metrics.export.prometheus.EnablePrometheusScraping;
 
+
 @EnablePrometheusMetrics
 @EnablePrometheusScraping
 @SpringBootApplication
@@ -13,4 +14,5 @@ public class PrometheusApplication {
     public static void main(String[] args) {
         SpringApplication.run(PrometheusApplication.class);
     }
+
 }
